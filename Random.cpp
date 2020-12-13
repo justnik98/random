cbd3f2fd37971334edb3f9daf9c74a9a89c97c25
Nikita <yannik98@yandex.ru>
2020-12-13 22:29:28 +0300

    skeleton code

---
+++ Random.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justnik on 13.12.2020.
+//
+
+#include "Random.h"
---
+++ Random.h
@@ -0,0 +1,14 @@
+//
+// Created by justnik on 13.12.2020.
+//
+
+#ifndef RANDOM_RANDOM_H
+#define RANDOM_RANDOM_H
+
+
+class Random {
+
+};
+
+
+#endif //RANDOM_RANDOM_H
