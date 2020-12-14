3344ed8a9cdcf9dc93779ad5a971114b6dbc3e9d
Nikita <yannik98@yandex.ru>
2020-12-14 18:43:39 +0300

    fix example of randExp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 
 int main() {
     Random r;
+    double mean = 1;
     std::cout << r.rand() << std::endl;
     std::cout << r.rnd() << std::endl;
     std::cout << r.randGaussian() << std::endl;
-    std::cout << r.randExp() << std::endl;
+    std::cout << r.randExp(mean) << std::endl;
     return 0;
 }
