c5258dd72060c90ca0407651adf740bb6fd98474
Nikita <yannik98@yandex.ru>
2020-12-17 01:49:50 +0300

    code refactoring

--- utils.h
+++ utils.h
@@ -5,23 +5,27 @@
 #ifndef RANDOM_UTILS_H
 #define RANDOM_UTILS_H
 
-#include <iterator>
+#include <numeric>
 
 namespace stat {
     template<class It>
-    double min(It first, It last);
+    double min(It first, It last) {
+        return std::accumulate(first, last, std::numeric_limits<double>::max(), std::min<double>);
+    }
 
     template<class It>
-    double max(It first, It last);
+    double max(It first, It last) {
+        return std::accumulate(first, last, std::numeric_limits<double>::min(), std::max<double>);
+    }
 
     template<class It>
-    double sum(It first, It last);
+    double sum(It first, It last) {
+        return std::accumulate(first, last, 0.0);
+    }
 
     template<class It>
-    double mean(It first, It last);
+    double mean(It first, It last) {
+        return static_cast<double>(sum(first, last)) / (last - first);
+    }
 }
-
-#include "utils.ipp"
-
-
 #endif //RANDOM_UTILS_H
